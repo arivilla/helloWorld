ddc3a3fdc4cc1e02b2bcc0837bc9fc446dc40be7
arivilla <33496527+arivilla@users.noreply.github.com>
2017-11-09 09:11:42 +0100

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author arivi
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo. Programando en java.");
+	}
+
+}
